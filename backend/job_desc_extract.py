f267c4825cfb75606cda8f995e677619b2829a52
Ajay Somala <jaydeveloper010@gmail.com>
2025-07-30 12:28:15 +0530

    Removed Local testing File Reference

--- backend/job_desc_extract.py
+++ backend/job_desc_extract.py
@@ -15,14 +15,3 @@ def extract_jd_keywords(text):
     keywords = [word for word in words if word not in stopwords and len(word) > 2]
 
     return list(set(keywords))  # unique keywords
-
-
-file3_path = r"D:\100%Workouts\ATS_Resume_Folder\documents\jd_text.txt"
-
-with open(file3_path, 'r', encoding='utf-8') as f:
-    jd_text = f.read()
-
-jd_keywords = extract_jd_keywords(jd_text)
-
-# print("Extracted JD Keywords:", jd_keywords)
-# print("Total Keywords:", len(jd_keywords))
